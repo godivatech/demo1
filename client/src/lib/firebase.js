773e5dc488dd6ed2ca2aedc6aaff122213714078
ajithyaasaf <ajithkannan.ag@gmail.com>
2025-04-29 15:17:24 +0530

    Config change

--- client/src/lib/firebase.js
+++ client/src/lib/firebase.js
@@ -6,14 +6,14 @@ import { getDatabase } from "firebase/database";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCMOdvQW248_4ul-ciFEEmuOghb4xdS3gs",
-  authDomain: "jpfinserv-892e1.firebaseapp.com",
-  databaseURL: "https://jpfinserv-892e1-default-rtdb.firebaseio.com",
-  projectId: "jpfinserv-892e1",
-  storageBucket: "jpfinserv-892e1.firebasestorage.app",
-  messagingSenderId: "166323272116",
-  appId: "1:166323272116:web:f440b4c76307ea463c5ae1",
-  measurementId: "G-GBS93JTW1R"
+  apiKey: "AIzaSyA_z0qWOAl1UIKd-eVP_9Q-o24tYu97T3k",
+  authDomain: "om-vinayaga-associates.firebaseapp.com",
+  databaseURL: "https://om-vinayaga-associates-default-rtdb.firebaseio.com",
+  projectId: "om-vinayaga-associates",
+  storageBucket: "om-vinayaga-associates.firebasestorage.app",
+  messagingSenderId: "275733848538",
+  appId: "1:275733848538:web:9528676a7fbd29190da0e3",
+  measurementId: "G-06H69CNSS1",
 };
 
 // Initialize Firebase
--- server/firebase.config.ts
+++ server/firebase.config.ts
@@ -1,13 +1,23 @@
 export const getFirebaseConfig = () => {
   // Use environment variables if available, otherwise use hardcoded values (development only)
   return {
-    apiKey: process.env.FIREBASE_API_KEY || "AIzaSyCMOdvQW248_4ul-ciFEEmuOghb4xdS3gs",
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN || "jpfinserv-892e1.firebaseapp.com",
-    databaseURL: process.env.FIREBASE_DATABASE_URL || "https://jpfinserv-892e1-default-rtdb.firebaseio.com",
-    projectId: process.env.FIREBASE_PROJECT_ID || "jpfinserv-892e1",
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "jpfinserv-892e1.firebasestorage.app",
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "166323272116",
-    appId: process.env.FIREBASE_APP_ID || "1:166323272116:web:f440b4c76307ea463c5ae1",
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-GBS93JTW1R"
+    apiKey:
+      process.env.FIREBASE_API_KEY || "AIzaSyA_z0qWOAl1UIKd-eVP_9Q-o24tYu97T3k",
+    authDomain:
+      process.env.FIREBASE_AUTH_DOMAIN ||
+      "om-vinayaga-associates.firebaseapp.com",
+    databaseURL:
+      process.env.FIREBASE_DATABASE_URL ||
+      "https://om-vinayaga-associates-default-rtdb.firebaseio.com",
+    projectId: process.env.FIREBASE_PROJECT_ID || "om-vinayaga-associates",
+    storageBucket:
+      process.env.FIREBASE_STORAGE_BUCKET ||
+      "om-vinayaga-associates.firebasestorage.app",
+    messagingSenderId:
+      process.env.FIREBASE_MESSAGING_SENDER_ID || "275733848538",
+    appId:
+      process.env.FIREBASE_APP_ID ||
+      "1:275733848538:web:9528676a7fbd29190da0e3",
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID || "G-06H69CNSS1",
   };
 };
